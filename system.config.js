f62db152b6fe1c68ef69179b0e23f7d85142d1e6
Philipp Dreher <dreher.philipp@googlemail.com>
2016-07-16 10:54:25 +0200

    SystemJS basics

---
+++ system.config.js
@@ -0,0 +1,17 @@
+System.config({
+    baseURL: './src/',
+    paths: {
+        "*": "*.js"
+    },
+    meta: {
+        'jquery': {
+            build: false
+        },
+        'hammerjs': {
+            build: false
+        },
+        'pnotify': {
+            build: false
+        }
+    }
+});
